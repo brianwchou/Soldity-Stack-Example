3c09b6384174e1e8eb235be46d10e4ea695c106c
brian <bchou515@gmail.com>
2019-08-08 23:22:24 -0400

    changed name of array to data

--- contracts/Stack.sol
+++ contracts/Stack.sol
@@ -1,24 +1,24 @@
 pragma solidity >=0.4.25 <0.6.0;
 
 contract Stack {
-    uint[] private array;
+    uint[] private data;
     uint private counter = 0;
 
     function peek() public view returns(uint) {
         require(counter > 0, "stack needs to have a value");
 
-        return array[counter - 1];
+        return data[counter - 1];
     }
 
     function pop() public {
         require(counter > 0, "stack needs to have values");
 
-        delete array[counter - 1];
+        delete data[counter - 1];
         counter -= 1;
     }
 
     function push(uint _value) public {
-        array.push(_value);
+        data.push(_value);
         counter += 1;
     }
 

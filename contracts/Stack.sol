777b6b350055234dd9e919702818fcbd81f1788c
brian <bchou515@gmail.com>
2019-08-11 23:00:35 -0400

    stack events

--- contracts/Stack.sol
+++ contracts/Stack.sol
@@ -3,6 +3,9 @@ pragma solidity >=0.4.25 <0.6.0;
 contract Stack {
     uint256[] private data;
 
+    event Push(uint256 value);
+    event Pop(uint256 value);
+
     function peek() public view returns(uint256) {
         require(data.length > 0, "stack needs to have a value");
 
@@ -12,10 +15,14 @@ contract Stack {
     function pop() public {
         require(data.length > 0, "stack needs to have values");
 
+        emit Push(data[data.length - 1]);
+
         data.length -= 1;
     }
 
     function push(uint256 _value) public {
+        emit Pop(_value);
+
         data.push(_value);
     }
 

815f6e4b250200fa997102c235b7a0b5d87910c0
Ethan Wessel <ejwessel@gmail.com>
2019-08-16 22:34:01 -0400

    test structure

--- test/Stack.test.js
+++ test/Stack.test.js
@@ -20,60 +20,71 @@ contract('Stack', async () => {
         await timeUtil.revertToSnapShot(snapshotId)
     })
 
-    describe('test stack initialization', async() => {
-        
-        it('peek() reverts with stack empty', async() => {
-            await truffleAssert.reverts(stack.peek(), 'stack needs to have a value')
-        })
-
-        it('push() increments size of stack', async() => {
-            let tx = await stack.push(5)
-            let value = await stack.getSize()
-    
-            truffleAssert.eventEmitted(tx, 'PushEvent', ev => {
-                return ev.value.toNumber() === 5 
-            }, "PushEvent should be emitted with correct parameters")
-    
-            assert.equal(value.toNumber(), 1, 'value was not pushed')
-        })
-
-        it('pop() fails with stack empty', async() => {
-            await truffleAssert.reverts(stack.pop(), 'stack needs to have values')
-        })
-
-        it('getSize() returns length with stack empty', async() => {
-            let value = await stack.getSize()
-            assert.equal(value.toNumber(), 0, 'stack is not empty')
-        })
+    it('test peek() with empty stack', async() => {
+        await truffleAssert.reverts(stack.peek.call(), 'stack needs to have a value')
     })
 
-    describe('test stack containing values', async() => {
+    it('test push() with empty stack', async() => {
+        let tx = await stack.push(5)
+        let value = await stack.getSize.call()
 
-        it('peek() retrieves pushed value', async() => {
-            await stack.push(5)
-            let value = await stack.peek()
-            assert.equal(value.toNumber(), 5, 'stack needs to have values')
-        })
-    
-        it('pop() removes top', async() => {
-            await stack.push(5)
-            let size = await stack.getSize()
-            assert.equal(size.toNumber(), 1, 'stack needs to have values')
-    
-            let tx = await stack.pop()
-            truffleAssert.eventEmitted(tx, 'PopEvent', ev => {
-                return ev.value.toNumber() === 5 
-            }, "PopEvent should be emitted with correct parameters")
-    
-            size = await stack.getSize()
-            assert.equal(size.toNumber(), 0, 'stack should be empty after pop')
-        })
+        truffleAssert.eventEmitted(tx, 'PushEvent', ev => {
+            return ev.value.toNumber() === 5 
+        }, "PushEvent should be emitted with correct parameters")
 
-        it('getSize() returns length with items in stack', async() => {
-            await stack.push(5)
+        assert.equal(value.toNumber(), 1, 'value was not pushed')
+    })
 
-            let value = await stack.getSize()
-            assert.equal(value.toNumber(), 1, `stack has ${value.toNumber()} items when it should have 1`)
-        })
+    it('test pop() with empty stack', async() => {
+        await truffleAssert.reverts(stack.pop(), 'stack needs to have values')
+    })
+
+    it('test getSize() with empty stack', async() => {
+        let value = await stack.getSize.call()
+        assert.equal(value.toNumber(), 0, 'stack is not empty')
+    })
+
+    it('test peek() with non-empty stack', async() => {
+        await stack.push(5)
+        let value = await stack.peek.call()
+        assert.equal(value.toNumber(), 5, 'stack needs to have values')
+    })
+
+    it('test push() with non-empty stack', async() => {
+        let tx = await stack.push(5)
+        let value = await stack.getSize.call()
+        truffleAssert.eventEmitted(tx, 'PushEvent', ev => {
+            return ev.value.toNumber() === 5 
+        }, "PushEvent should be emitted with correct parameters")
+        assert.equal(value.toNumber(), 1, 'value was not pushed')
+
+        tx = await stack.push(6)
+        value = await stack.getSize.call()
+        truffleAssert.eventEmitted(tx, 'PushEvent', ev => {
+            return ev.value.toNumber() === 6 
+        }, "PushEvent should be emitted with correct parameters")
+        assert.equal(value.toNumber(), 2, 'value was not pushed')
+    })
+
+    it('test pop() with non-empty stack', async() => {
+        await stack.push(5)
+        let size = await stack.getSize.call()
+        assert.equal(size.toNumber(), 1, 'stack needs to have values')
+
+        let tx = await stack.pop()
+
+        truffleAssert.eventEmitted(tx, 'PopEvent', ev => {
+            return ev.value.toNumber() === 5 
+        }, "PopEvent should be emitted with correct parameters")
+
+        size = await stack.getSize.call()
+        assert.equal(size.toNumber(), 0, 'stack should be empty after pop')
+    })
+
+    it('test getSize() with non-empty stack', async() => {
+        await stack.push(5)
+
+        let value = await stack.getSize.call()
+        assert.equal(value.toNumber(), 1, `stack has ${value.toNumber()} items when it should have 1`)
     })
 })
